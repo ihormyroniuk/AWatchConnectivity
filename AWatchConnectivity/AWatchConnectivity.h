5566848014a9af587dc0df25860797ed604ec655
Ihor Myroniuk <ihormyroniuk@gmail.com>
2023-12-17 00:27:09 +0200

    no message

---
+++ AWatchConnectivity/AWatchConnectivity.h
@@ -0,0 +1,18 @@
+//
+//  AWatchConnectivity.h
+//  AWatchConnectivity
+//
+//  Created by Ihor Myroniuk on 17.12.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AWatchConnectivity.
+FOUNDATION_EXPORT double AWatchConnectivityVersionNumber;
+
+//! Project version string for AWatchConnectivity.
+FOUNDATION_EXPORT const unsigned char AWatchConnectivityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AWatchConnectivity/PublicHeader.h>
+
+
